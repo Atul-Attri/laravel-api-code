c27c18c798c446a42d05d922e04d8ffb25541905
AtulAttri-24 <atul.sharma012494@gmail.com>
2020-08-08 16:15:52 +0530

    message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,15 +23,19 @@
             .full-height {
                 height: 100vh;
             }
-
+    
             .flex-center {
                 align-items: center;
                 display: flex;
                 justify-content: center;
+
+
+
             }
 
             .position-ref {
                 position: relative;
+justify-content: center;
             }
 
             .top-right {
